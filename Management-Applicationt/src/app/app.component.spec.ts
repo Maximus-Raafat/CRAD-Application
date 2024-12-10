8622c00ae54fed7cdf687f0f3d84d276a4818dc4
Maximus <79494898+Maximus-Raafat@users.noreply.github.com>
2024-12-10 12:39:00 +0200

    initial

--- Management-Applicationt/src/app/app.component.spec.ts
+++ Management-Applicationt/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterModule.forRoot([])
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Management-Application' title`, () => {
+  it(`should have as title 'Management-Applicationt'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Management-Application');
+    expect(app.title).toEqual('Management-Applicationt');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Management-Application');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Management-Applicationt');
   });
 });
